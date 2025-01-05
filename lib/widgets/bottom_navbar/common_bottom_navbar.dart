5acaa21a6bccfdc16b4aeb5c9e745d9e71f7895e
Krishna Suryavanshi <krishna.suryavanshiac@gmail.com>
2025-01-05 18:09:42 +0530

    made some changes in features with late arrival as greyed out.

--- lib/widgets/bottom_navbar/common_bottom_navbar.dart
+++ lib/widgets/bottom_navbar/common_bottom_navbar.dart
@@ -116,15 +116,41 @@ class _NewBottomBarState extends State<NewBottomBar> {
             top: -30, // Adjust this value to move the icon up or down
             left: MediaQuery.of(context).size.width / 2 -
                 35, // Center the icon horizontally
-            child: InkWell(
-              onTap: () {
-                Get.to(
-                  () => const PoolOffersScreen(),
-                );
-              },
-              child: const SvgIcon(
-                "assets/bottombar/live.svg",
-                size: 70, // Size of the center icon
+            child: const InkWell(
+              onTap: null
+              //  () {
+              // Get.to(
+              //   () => const PoolOffersScreen(),
+              // );
+              // }
+              ,
+              child: ColorFiltered(
+                colorFilter: ColorFilter.matrix(<double>[
+                  0.2126,
+                  0.7152,
+                  0.0722,
+                  0,
+                  0,
+                  0.2126,
+                  0.7152,
+                  0.0722,
+                  0,
+                  0,
+                  0.2126,
+                  0.7152,
+                  0.0722,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                  0,
+                ]),
+                child: SvgIcon(
+                  "assets/bottombar/live.svg",
+                  size: 70, // Size of the center icon
+                ),
               ),
             ),
           ),
